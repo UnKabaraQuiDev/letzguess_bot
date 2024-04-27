b428d42cb785f2bd3343a1c56f17b113cdf95d5d
P.Cy.113 <poucy113@github.com>
2024-04-27 11:30:44 +0200

    Cleaning up branches

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # letzguess_bot
 
-Add you token & cookies in `answers.json`
+A bot for playing LetzGuess
+- - -
+
+## Python version:
+This version does not work anymore, it will probably get you banned.
+
+## Tampermonkey version:
+* Tested in Firefox with Tampermonkey v5.1.0
+### Installation:
+1. Install the `Tampermonkey` plug-in
+2. Go to [https://www.letzguess.lu/](https://www.letzguess.lu/)
+3. Create a new Tampermonkey script from the top bar (Tampermonkey>Create new script)
+4. Copy the content of [main.js](https://github.com/UnKabaraQuiDev/letzguess_bot/blob/main/tampermonkey_js/main.js) into the new script
+5. Save an go back to LetzGuess [LetzGame > Game > Challenge](https://www.letzguess.lu/game/challenge)
+6. Refresh the page to enable the script
+* Press `F` to auto-fill the correct answer (if saved)
+* Press `Space` to continue to the next question.
+<br>
+*Note: This scripts will make a lot of mistakes at the beginning but will gradually become better as it saves the correct answers.*
+### Settings:
+There are a few settings in the tampermonkey script file:
+* `random_failure`: The percentage rate to make a mistake even if the answer is known.
+* `max_streak`: Limit the maximum correct answer streak.
+* `no_error`: Override `random_failure`.
+
---
+++ tampermonkey_js/main.js
@@ -0,0 +1,186 @@
+// ==UserScript==
+// @name     Letzguess help
+// @version  1
+// @grant    GM.getValue
+// @grant    GM.setValue
+// @match  https://www.letzguess.lu/game/challenge*
+// @run-at   document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var round_env = undefined;
+    var question_id = undefined;
+    var saved_answer = undefined;
+
+    function get_available_answers() {
+        if(round_env === undefined) {
+            round_env = document.querySelector('.game-challenge-round');
+            if (!round_env) {
+                console.log("No element with class .game-challenge-round found");
+                return;
+            }
+        }
+
+        const roundAnswers = round_env.querySelector('.game-challenge-round-responses');
+        if (roundAnswers) {
+            const innerHTMLValues = {};
+
+            roundAnswers.querySelectorAll("button").forEach(child => {
+                if (child.nodeType === Node.ELEMENT_NODE) {
+                    innerHTMLValues[child.textContent] = child;
+                }
+            });
+            //console.log(innerHTMLValues);
+            return innerHTMLValues;
+        } else {
+            console.log("No element with class .game-challenge-round-responses found inside .game-challenge-round");
+        }
+        return undefined;
+    }
+
+    function click(buttonToClick) {
+        console.log("click", buttonToClick);
+        if(!buttonToClick) {
+            console.log("Button is null");
+            return;
+        }
+        buttonToClick.click();
+        return;
+    }
+
+    function get_random_element_exclude(array, exclude) {
+        var keys = Object.keys(array);
+        delete keys[exclude];
+        return array[keys[Math.floor(Math.random() * keys.length)]];
+    }
+
+    function get_random_element(array) {
+        var keys = Object.keys(array);
+        return array[keys[Math.floor(Math.random() * keys.length)]];
+    }
+
+    function get_random_element_array(array) {
+        return array[Math.floor(Math.random() * array.length)];
+    }
+
+    function get_current_streak() {
+        const parentElement = Array
+        .from(document.querySelectorAll('p'))
+        .find(el => el.innerHTML === 'Streak').parentNode;
+        return parseInt(parentElement.querySelector('h2').innerHTML);
+    }
+
+    function next() {
+        const nextBtn = document.querySelector("div.game-challenge-round-next > button");
+        click(nextBtn);
+    }
+
+
+    const random_failure = 28; // /100%
+    const max_streak = 160;
+    const no_error = true;
+
+    // Function to run when the shortcut is pressed
+    async function runScript() {
+        var buttons = get_available_answers();
+        console.log("buttons:", buttons);
+
+        if(saved_answer == undefined) {
+            // well well
+            console.log("Well well");
+        }else {
+            click(buttons[saved_answer]);
+        }
+
+    }
+
+    async function saveResponse(question_id, correct_answer) {
+        console.log("setting: ", question_id, correct_answer);
+        if(correct_answer === undefined || saved_answer === "undefined")
+            return;
+        await GM.setValue(question_id, correct_answer);
+    }
+
+    async function loadResponse(question_id, available_answers, current_streak) {
+        saved_answer = await GM.getValue(question_id);
+
+        if(saved_answer === undefined || saved_answer === "undefined") {
+            console.log("No saved answer found for: ", question_id);
+
+            saved_answer = get_random_element_array(available_answers);
+
+            console.log("Random: ", saved_answer);
+        }else {
+            console.log("Saved answer found for: ", question_id, saved_answer);
+        }
+
+        if (Math.random()*100 < random_failure && !no_error) {
+            saved_answer = get_random_element_array(available_answers);
+            console.log("Changed answer, random = ", saved_answer);
+        }
+
+        const streak = current_streak;
+        console.log("Current streak: ", streak);
+        if (max_streak == streak) {
+            saved_answer = get_random_element_exclude(available_answers, saved_answer);
+            console.log("Changed answer, max_streak = ", saved_answer);
+        }
+    }
+
+    var originalOpen = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function(method, url) {
+        console.log('Outgoing HTTP request intercepted:', method, url);
+        // You can modify the request here if needed
+
+        // Store reference to the XMLHttpRequest object
+        var self = this;
+
+        // Attach an event listener for the load event
+        this.addEventListener('load', function() {
+            // Check if the response is JSON
+            var contentType = self.getResponseHeader('Content-Type');
+            //console.log("content ", url, self.responseText);
+            //console.log("Loaded: ", url);
+            if (contentType && contentType.indexOf('application/json') !== -1) {
+                if(!url.match(".+\/round\/([0-9]+)")) {
+                    return;
+                }
+
+                // Parse the JSON response
+                var jsonResponse = JSON.parse(self.responseText);
+                console.log('JSON Response:', url, jsonResponse);
+                // You can further process the JSON response here
+
+                if(jsonResponse.round.ended || jsonResponse.round.question.correct_response) {
+                    saveResponse(jsonResponse.round.question.question_id, jsonResponse.round.question.correct_response);
+                }else if(!jsonResponse.round.ended) {
+                    loadResponse(jsonResponse.round.question.question_id, jsonResponse.round.question.responses, jsonResponse.round.streak);
+                }
+            }
+        });
+
+        // Call the original open method
+        originalOpen.apply(this, arguments);
+    };
+
+
+    // Define the keyboard shortcut (e.g., Ctrl + Shift + F)
+    const shortcutKey = 'f';
+    // const modifierKeys = ['ctrlKey', 'shiftKey'];
+
+    GM.listValues().then(t => t.forEach(async v => console.log(v+": ", await GM.getValue(v))), b => console.log("Could not list values", b));
+
+    // Event listener for keydown event
+    document.addEventListener('keydown', function(event) {
+        // console.log(event);
+        if (event.key == shortcutKey/* && modifierKeys.every(key => event[key])*/) {
+            event.stopPropagation();
+            runScript();
+        }else if(event.key == ' ') {
+            event.stopPropagation();
+            next();
+        }
+    });
+})();
